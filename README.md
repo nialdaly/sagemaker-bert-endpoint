bbc2575e6430e16363156ac353c733acd9e87d41
nialdaly <nial.daly@outlook.com>
2020-10-16 17:46:02 +0100

    predictor.py created

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
 # SageMaker BERT Endpoint
-The following project will demonstrate the process of deploying a BERT model fine-tuned on SQuAD. The model will be deployed to a SageMaker PyTorch endpoint. This process makes use of Amazon SageMaker, CloudFormation, Elastic Container Service (ECS) and the HuggingFace library.
+The following project will demonstrate the process of deploying a BERT model fine-tuned on SQuAD dataset provided by HuggingFace. The model will be deployed to a SageMaker PyTorch endpoint. This process makes use of Amazon SageMaker, CloudFormation, Elastic Container Service (ECS) and the HuggingFace transformers library.
 
 ## Prerequisites
+AWS Account with an IAM (SageMaker Execution) Role
+
+## Getting Started
+The easiest way to get started is to spin up an Amazon SageMaker notebook instance and clone the repository via the following command.
+```git clone https://github.com/nialdaly/sagemaker-bert-endpoint.git
+```
--- predictor/predictor.py
+++ predictor/predictor.py
@@ -2,40 +2,22 @@ import torch
 import os
 import json
 from transformers import BertTokenizer, BertForQuestionAnswering, BertConfig
-import logging
 import sys
-
-# Model Logging
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.DEBUG)
-logger.addHandler(logging.StreamHandler(sys.stdout))
-
-logger.info('reached inference script')
-logger.info(os.listdir('/opt/ml/model'))
-
-logger.info(os.listdir('/opt/ml/model/code'))
-
 
 JSON_CONTENT_TYPE = 'application/json'
 
 def model_fn(model_dir):
-    logger.info('reached model_fn')
-    config_path=model_dir+'/config_file.json'
-    model_path=model_dir+'/pytorch_model.bin'
-    
-
-#     config_path=model_dir+'/model/config_file.json'
-#     model_path=model_dir+'/model/pytorch_model.bin'
+    config_path = model_dir+'/config_file.json'
+    model_path = model_dir+'/pytorch_model.bin'
     
     config = BertConfig.from_json_file(config_path)
     model = BertForQuestionAnswering(config)
-
+    
+    # Checks GPU state
     model.load_state_dict(torch.load(model_path, map_location=torch.device('cuda' if torch.cuda.is_available() else 'cpu')))
     return model
 
 def input_fn(serialized_input_data, content_type=JSON_CONTENT_TYPE):
-    logger.info('reached input_fn')
-
     if content_type == JSON_CONTENT_TYPE:
         input_data = json.loads(serialized_input_data)
         return input_data
@@ -45,9 +27,6 @@ def input_fn(serialized_input_data, content_type=JSON_CONTENT_TYPE):
         return
 
 def predict_fn(input_data, model):
-    logger.info('reached predict_fn')
-
-    logger.info(os.listdir('/opt/ml/model'))
     vocab_path = '/opt/ml/model/vocab.txt'
     tokenizer = BertTokenizer(vocab_path, do_lower_case=True)
 
@@ -55,7 +34,7 @@ def predict_fn(input_data, model):
     
     input_ids = tokenizer.encode(question, context)
     token_type_ids = [0 if i <= input_ids.index(102) else 1 for i in range(len(input_ids))]
-    start_scores, end_scores = model(torch.tensor([input_ids]), token_type_ids=torch.tensor([token_type_ids]))
+    start_scores, end_scores = model(torch.tensor([input_ids]), token_type_ids = torch.tensor([token_type_ids]))
     all_tokens = tokenizer.convert_ids_to_tokens(input_ids)
     answer = ' '.join(all_tokens[torch.argmax(start_scores) : torch.argmax(end_scores)+1])
     return answer
